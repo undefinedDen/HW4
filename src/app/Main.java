91528095a2a92eab895bfb0868c1a138bd489d91
undefinedDen <tunevdenis123@gmail.com>
2024-12-07 03:36:39 +0200

    initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static final String CURRENCY = "EUR";
+   static double totalSum;
+   static double daySum;
+   static double price;
+   static int day;
+   static int quantityProd;
+   static String product;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
